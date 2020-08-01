3fa8c5eeb3c4d9b75372ee957d60836bd056b9a8
Jeyanth Chandru <jeyanth.c2008@gmail.com>
2020-08-01 16:02:39 -0500

    V5 Migration Part 3
    Migrated Service Files

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Users } from '../models/users/users.model';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private auth: AngularFireAuth) {
+  }
+
+  getUID() {
+    return this.auth.currentUser;
+  }
+
+  registerUser(user: Users) {
+    return this.auth.createUserWithEmailAndPassword(user.email, user.password);
+  }
+
+  loginUser(user: Users) {
+    return this.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
+
+  logoutUser(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.auth.signOut()
+        .then((data: any) => {
+          resolve(data);
+        })
+        .catch((error: any) => {
+          reject(error);
+        });
+    });
+  }
+
+  getEmail() {
+    return this.auth.currentUser.then(userInfo => userInfo.email)
+  }
+}
---
+++ src/app/service/box.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Box } from '../models/box-model/box.model';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoxService {
+
+  box: Observable<Box[]>
+  dataRef: AngularFireList<Box>;
+  moveKey: string;
+  uid: string;
+  sharedKey: string;
+  sharedMoveKey: string;
+  constructor(private db: AngularFireDatabase) {
+  }
+
+  getBoxDetails(uid, key) {
+    this.dataRef = this.db.list<Box>(uid + '/move/' + key + '/box');
+    this.moveKey = key;
+    this.uid = uid;
+    this.box = this.dataRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ $key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+    return this.box;
+  }
+
+  getSharedBoxDetails(sharedKey, key) {
+    this.dataRef = this.db.list<Box>('shared/' + sharedKey + '/' + key + '/box');
+    this.sharedMoveKey = key;
+    this.sharedKey = sharedKey;
+    this.box = this.dataRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ $key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+    return this.box;
+  }
+
+  createBox() {
+    return this.db.database.ref(this.uid + '/move/' + this.moveKey + '/box').push().key
+  }
+
+  createSharedBox() {
+    return this.db.database.ref('shared/').child(this.sharedKey).child(this.sharedMoveKey).child('Box').push().key
+    // return this.db.database.ref('shared/' + this.sharedKey + '/' + this.sharedMoveKey + '/box/').push().key
+  }
+
+  updateBox(key, tempBox: Box) {
+    this.dataRef.update(key, tempBox);
+  }
+
+  // updateSharedBox(key, tempBox: Box) {
+  //   this.dataRef.update(key, tempBox);
+  // }
+
+  addBoxToDB(key, tempBox: Box) {
+    this.db.database.ref(this.uid + '/move/' + this.moveKey + '/box').child(key).set(tempBox);
+  }
+
+  addSharedBoxToDB(key, tempBox: Box) {
+    this.db.database.ref('shared/' + this.sharedKey + '/' + this.sharedMoveKey + '/box').child(key).set(tempBox);
+  }
+
+  removeBox(key) {
+    this.dataRef.remove(key);
+  }
+
+  getMoveKey() {
+    return this.moveKey;
+  }
+
+  getSharedMoveKey() {
+    return this.sharedMoveKey;
+  }
+}
